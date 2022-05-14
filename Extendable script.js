2cac658d7b72aa250854824ea94faa17cff9c35f
Revell-Eden <102336484+Revell-Eden@users.noreply.github.com>
2022-05-14 16:05:00 +1000

    Extendable content overlapping resolved
    Extendable content now resides within its parent div allowing for the later addition of animation. This will consist of a folding element that will expand and fold to fit the page.

--- Extendable script.js
+++ Extendable script.js
@@ -1,26 +1,35 @@
 function toggleContent1() {
     var x = document.getElementById("hidden1");
-    if (x.style.display === "block") {
+    var y = document.getElementById("hiddenAnim1")
+    if (x.style.display === "block"){
       x.style.display = "none";
+      y.style.display = "none";
     } else {
       x.style.display = "block";
-    }
-  }
+      y.style.display = "block"; 
+}   
+}
 
-  function toggleContent2() {
+function toggleContent2() {
     var x = document.getElementById("hidden2");
-    if (x.style.display === "block") {
+    var y = document.getElementById("hiddenAnim2")
+    if (x.style.display === "block"){
       x.style.display = "none";
+      y.style.display = "none";
     } else {
       x.style.display = "block";
-    }
-  }
- 
-  function toggleContent3() {
+      y.style.display = "block"; 
+}   
+}
+
+function toggleContent3() {
     var x = document.getElementById("hidden3");
-    if (x.style.display === "block") {
+    var y = document.getElementById("hiddenAnim3")
+    if (x.style.display === "block"){
       x.style.display = "none";
+      y.style.display = "none";
     } else {
       x.style.display = "block";
-    }
-  }
+      y.style.display = "block"; 
+}   
+}
