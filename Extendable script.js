749c72dd4fe6cdcf5126f831883290d96006162d
Revell-Eden <102336484+Revell-Eden@users.noreply.github.com>
2022-05-14 13:10:09 +1000

    Add Accordion inspired extendable content list
    Accordion dropdowns allow the user to choose a morsel of content that they desire to have priority on the viewport. 
    This embeds the dynamic design into the webpage.
    External Javascript handles the extendable action of the hidden content, employing a simple class change and conditional application.

---
+++ Extendable script.js
@@ -0,0 +1,26 @@
+function toggleContent1() {
+    var x = document.getElementById("hidden1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function toggleContent2() {
+    var x = document.getElementById("hidden2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+ 
+  function toggleContent3() {
+    var x = document.getElementById("hidden3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
